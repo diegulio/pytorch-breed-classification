2648d3350d2ab327894f5d36bbbdd715f2227de2
diegulio <diego.machado@usach.cl>
2023-10-20 16:35:52 -0300

    feat (config): add config

--- app/config.py
+++ app/config.py
@@ -1,23 +1,33 @@
 import pandas as pd
 
-IMG_PATH = '/content/drive/MyDrive/Colab Data/buscomiperro/images'
-LABEL_PATH = '/content/drive/MyDrive/Colab Data/buscomiperro/labels.csv'
+class CFG:
+    SEED = 13
+    
+    IMG_PATH = '/content/drive/MyDrive/Colab Data/buscomiperro/images'
+    LABEL_PATH = '/content/drive/MyDrive/Colab Data/buscomiperro/labels.csv'
 
-labels = pd.read_csv(LABEL_PATH)
-idx_to_class = dict(enumerate(labels.breed.unique()))
-class_to_idx = {c:i for i,c in idx_to_class.items()}
+    labels = pd.read_csv(LABEL_PATH)
+    idx_to_class = dict(enumerate(labels.breed.unique()))
+    class_to_idx = {c:i for i,c in idx_to_class.items()}
 
 
-# Model related
-LEARNING_RATE = 0.001
-MODEL = 'inception_v4'
-PRETRAINED = True
-PRECISION = 16
-MIN_EPOCHS = 1
-MAX_EPOCHS = 20
-ACCELERATOR = 'gpu'
-AUGMENTATION = True
+    # Model related
+    LEARNING_RATE = 0.001
+    MODEL = 'inception_v4'
+    PRETRAINED = True
+    PRECISION = 16
+    MIN_EPOCHS = 1
+    MAX_EPOCHS = 20
+    ACCELERATOR = 'gpu'
+    AUGMENTATION = True
+    MODEL_PATH = "model.pt"
+    
+    # Data
+    TEST_SIZE = 0.2
+    VAL_SIZE = 0.1
+    BATCH_SIZE = 64
+    IMG_SIZE = (299, 299) # Depends in base model
 
-# Wandb related
-WANDB_PROJECT = 'breed-classification-pytorch'
-WANDB_ENTITY = 'diegulio'
+    # Wandb related
+    WANDB_PROJECT = 'breed-classification-pytorch'
+    WANDB_ENTITY = 'diegulio'
